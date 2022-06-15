6d66b4c6116d1544e34b373db4a87448b7d35c9a
99positive <1047969817@qq.com>
2022-06-15 10:36:52 +0800

    eureka client

---
+++ com.lqb.springcloud_parent/springcloud_02eureka_client/src/main/java/com/lqb/EurekaClientApplication.java
@@ -0,0 +1,17 @@
+package com.lqb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Date 2022/6/14 17:46
+ * @Create by lqb
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
