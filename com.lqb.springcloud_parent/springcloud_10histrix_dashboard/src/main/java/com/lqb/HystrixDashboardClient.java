7cb1d4f15d020467982448e2f4516ca36d122e9a
99positive <1047969817@qq.com>
2022-07-03 21:48:16 +0800

    histrix dashboard

--- com.lqb.springcloud_parent/springcloud_10histrix_dashboard/src/main/java/com/lqb/HystrixDashboardClient.java
+++ com.lqb.springcloud_parent/springcloud_10histrix_dashboard/src/main/java/com/lqb/HystrixDashboardClient.java
@@ -4,20 +4,21 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
 /**
- * @Date 2022/7/2 15:21
+ * @Date 2022/7/3 2:44
  * @Create by lqb
  */
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker //开启Hystrix服务熔断
-public class HystrixClient {
+//@EnableDiscoveryClient 引入依赖后自动会注册：consul zk nacos
+@EnableHystrixDashboard // 开启仪表盘应用
+public class HystrixDashboardClient {
+
     public static void main(String[] args) {
-        SpringApplication.run(HystrixClient.class, args);
+        SpringApplication.run(HystrixDashboardClient.class, args);
     }
 
     @Bean
